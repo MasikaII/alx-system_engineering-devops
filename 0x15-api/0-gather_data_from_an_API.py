a35067625bc167a54932234a3d41fdc5937045f1
Stephy <atienostephanie040@gmail.com>
2023-01-16 14:18:54 +0000

    Using a REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,31 @@
 #!usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Using https://jsonplaceholder.typicode.com
+returns info about employee TODO progress
+Implemented using recursion
+"""
+import json
+from sys import argv
 import requests
-import sys
+
+
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(f'{API}/users/{argv[1]}').json()
+    todos = requests.get(f'{API}/todos', params={"userId": argv[1]}).json()
+    total_tasks = len(todos)
+    completed = 0
+    completed_tasks = []
+    for todo in todos:
+        if todo["completed"]:
+            completed += 1
+            completed_tasks.append(todo["title"])
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    output = f'Employee {user["name"]} is\
+done with tasks ({completed}/{total_tasks}):'
+    print(output)
+    for task in completed_tasks:
+        print(f'\t {task}')
